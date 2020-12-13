09d59137f1ebfcd96792d156ae74ad281ca32c1e
MyaraB <74420571+MyaraB@users.noreply.github.com>
2020-12-13 19:50:11 +0200

    ex2 project ben and moshe

---
+++ Ex2/src/gui/Label.java
@@ -0,0 +1,38 @@
+package src.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class Label extends JFrame implements ActionListener {
+
+    JTextField af;
+    JLabel l;
+    JButton b;
+
+
+    public Label(){
+af = new JTextField();
+        af.setBounds(50,50,150,20);
+        l= new JLabel();
+        l.setBounds(50,100,250,20);
+        l.setText(("Insert url to find the IP"));
+        b= new JButton("find ip");
+        b.setBounds(50,150,95,30);
+        b.addActionListener(this);
+        this.add(b);
+        this.add(af);
+        this.add(l);
+        setSize(400,400);
+        setLayout(null);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    public static void main(String[] args) {
+        new Label();
+    }
+}
